d0fb2c9c958e98897e6763d85d54e6fc406b69ab
Abouzar Rostami <75476303+stormyboy68@users.noreply.github.com>
2025-07-01 23:41:28 +0330

    deploy

--- frontend/src/bootstrap/const.tsx
+++ frontend/src/bootstrap/const.tsx
@@ -7,14 +7,17 @@ export const VersionRoute_ADMIN = "/v1";
 export const VersionRoute_SUBSCRIPTION = "/v1";
 export const VersionRoute_TYPE = "/v1";
 
-export const BASE_URL = process.env.NODE_ENV === "production"
-    ?  process.env.YOUR_DOMAIN+":3000" 
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.YOUR_DOMAIN + ":3000"
     : "http://localhost";
 
-export const SERVER_URL = process.env.YOUR_DOMAIN+":8000/api";
-
+export const SERVER_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.YOUR_DOMAIN + ":8000/api"
+    : "http://localhost:8000/api";
 
 export const API = BASE_URL + "/api" + VersionRoute;
 
-export const REMEMBER_ME = 30
-export const DELAY_NEXT_RESEND_SMS = 10
+export const REMEMBER_ME = 30;
+export const DELAY_NEXT_RESEND_SMS = 10;
